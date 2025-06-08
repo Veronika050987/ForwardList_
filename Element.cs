c2aea509598fe084728ff9deada2e4597a5bd6b0
Veronika050987 <veronicaviv@mail.ru>
2025-06-08 09:11:20 +0300

    Add project files.

---
+++ Element.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForwardList
+{
+    class Element
+    {
+        public int Data { get; set; }
+        public Element pNext { get; set; }
+        public Element(int Data, Element pNext = null)
+        {
+            this.Data = Data;
+            this.pNext = pNext;
+            Console.WriteLine($"EConstructor:{GetHashCode()}");
+        }
+
+        ~Element()
+        {
+			Console.WriteLine($"EDestructor:{GetHashCode()}");
+		}
+	}
+}
